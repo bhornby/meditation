3bd7bac5b9f3d7f47e314a81315dd8d951aef45d
bhornby <70747776+bhornby@users.noreply.github.com>
2023-09-27 11:22:22 +0100

    unfortunately unable to finish and get the app hosted within 1 hour but will get it up in the next 30 mins once the sidebar is finished

--- main.py
+++ main.py
@@ -8,17 +8,33 @@ def set_stage(i):
     st.session_state.stage = i
 # end function
 
+def make_sidebar():
+    with st.sidebar:
+        st.title("Navigation")
+        st.markdown("#####")
+        st.button("Home", on_click=set_stage, args=[0], use_container_width=True)
+        st.button("How to Meditate", on_click=set_stage, args=["1"], use_container_width=True)  
+        st.button("Other Resourses", on_click=set_stage, args=["2"], use_container_width=True) 
+        st.button("Help", on_click=set_stage, args=[3],use_container_width=True)
+# end function
+
 def home_screen():
 
-    st.title("Meditate Me ")
-    st.divider()
-    
-    meditation_time = st.slider("Meditation Time (minutes)", 1, 90 ,5 )
-    break_time = st.slider("Break Time", 1, 30, 5)
+    if "stage" not in st.session_state:
+        st.session_state.stage = 0
 
-    if st.button("Start Meditation"):
-        timer(meditation_time, break_time)
-    
+    if st.session_state.stage == 0:
+        st.title("Meditate Me ")
+        st.divider()
+        make_sidebar()
+        
+        meditation_time = st.slider("Meditation Time (minutes)", 0, 90 ,5 )
+        break_time = st.slider("Break Time", 1, 30, 5)
+
+        if st.button("Start Meditation"):
+            timer(meditation_time, break_time)
+    # end if
+# end function
     
 
 def timer(meditation_time, break_time):
